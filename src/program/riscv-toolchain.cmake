1f7f4718b6387df8df3cd96aac8be6956b80bcd4
Gabriel Abarca <gababarcaa@gmail.com>
2026-06-17 23:53:41 -0600

    Update Dockerfile and documentation for experiment plan and references (#1)
    * Update Dockerfile and documentation for experiment plan and references
    
    - Added necessary LaTeX packages to Dockerfile for document compilation.
    - Introduced a new section in main.tex detailing the proposed solution and experiment plan.
    - Added reference for LeetCode problems used in the experiment.
    
    * Enhance performance metrics section by detailing execution scenarios and metrics for the proposed accelerator
    
    * Update Dockerfile and riscv-toolchain.cmake for RISC-V toolchain compatibility

--- src/program/riscv-toolchain.cmake
+++ src/program/riscv-toolchain.cmake
@@ -1,7 +1,12 @@
-set(CMAKE_SYSTEM_NAME Generic)
-set(CMAKE_SYSTEM_PROCESSOR riscv)
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR riscv64)
 
-set(CMAKE_C_COMPILER riscv64-unknown-elf-gcc)
-set(CMAKE_CXX_COMPILER riscv64-unknown-elf-g++)
+set(CMAKE_C_COMPILER riscv64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER riscv64-linux-gnu-g++)
+
+# Produce a self-contained RISC-V ELF (no dynamic loader needed).
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -static")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static")
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
